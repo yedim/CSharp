cbf0a6434e2a0d8acf9e973c4f7d8f2851eb10ed
Lee Ye Rim <yelimi0416@gmail.com>
2018-03-06 14:50:11 +0900

    static 메소드 호출 불가

--- Chapter01_02/Chapter01_02/Program.cs
+++ Chapter01_02/Chapter01_02/Program.cs
@@ -45,7 +45,7 @@ namespace Chapter01_02
             Console.WriteLine("{0}", res1);
 
             CMyClass tmpMC = new CMyClass();//일반 메소드는 객체가 있어야 호출가능
-            int res2 = tmpMC.Multiply(5, 6);
+            int res2 = tmpMC.Multiply(5, 6);//static 메소드는 호출불가
             Console.WriteLine("{0}", res2);
 
             Console.ReadKey();
