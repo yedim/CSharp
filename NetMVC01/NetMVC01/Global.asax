f740adc7b5c6243b981abdf86c33995190938baf
Lee Ye Rim <yelimi0416@gmail.com>
2018-04-09 10:59:26 +0900

    MVC 하이퍼링크와 model과 @Html

---
+++ NetMVC01/NetMVC01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetMVC01.MvcApplication" Language="C#" %>
---
+++ NetMVC01/NetMVC01/Models/CUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetMVC01.Models
+{
+    public class CUser
+    {
+        public int theUniqueID { get; set; } //property 
+        public string theID { get; set; }
+        public string thePW { get; set; }
+
+        public string theName { get; set; }
+        public string theEMail { get; set; }
+        public bool bSubscription { get; set; }
+
+        public DateTime theDate { get; set; }
+    }
+}
