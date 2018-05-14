215e99883a68f158d5324aab24502e9e634bcb1c
Lee Ye Rim <yelimi0416@gmail.com>
2018-05-14 10:47:14 +0900

    1차원,2차원 배열

---
+++ Chapter03_01/Chapter03_01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter03_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            numbers[0] = 10;
+            numbers[1] = 15;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+
+            int[] nums = new int[] { 2, 3, 5, 8, 13 };
+            Console.WriteLine(nums[0]);
+            Console.WriteLine(nums[3]);
+
+            int i;
+            Console.WriteLine();
+            Console.Write("Nums : ");
+            for (i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}, ", nums[i]);//배열은 index쓸 수 있으므로 자료의 범위 확장 
+            }
+
+            int[] num2s = { 3, 1, 4, 1, 5, 9, 2 };
+            Console.WriteLine();
+            Console.Write("num2s : ");
+            for (i = 0; i < num2s.Length;i++ )
+            {
+                Console.Write("{0}, ", num2s[i]);
+            }
+            Console.WriteLine();
+            Console.Write("num2s : ");
+            foreach(int iter in num2s)
+            {
+                Console.Write("{0}, ", iter);
+            }
+
+            String[] tmpStrs = { "C++", "C#", "Java", "Swift" };
+            Console.WriteLine();
+            Console.Write("tmpStrs : ");
+            foreach(String iter in tmpStrs)
+            {
+                Console.Write("{0}, ", iter);
+            }
+
+            String[,] tmpStrLists = { { "Apple", "Banana", "Lemon" }, { "Pizza", "Pasta", "Risoto" } };
+            Console.WriteLine();
+            Console.WriteLine(tmpStrLists[0, 1]);
+            Console.WriteLine(tmpStrLists[1, 2]);
+
+            int j;
+            for (i = 0; i < tmpStrLists.GetLength(0);i++ )
+            {
+                for(j=0; j<tmpStrLists.GetLength(1); j++)
+                {
+                    Console.WriteLine("{0}, ", tmpStrLists[i, j]);
+                }
+                Console.WriteLine();
+            }
+            foreach(String iter in tmpStrLists)
+            {
+                Console.WriteLine("{0}, ", iter);
+            }
+                Console.ReadKey();
+        }
+    }
+}
